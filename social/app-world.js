ba9b6f84aa3eeff906692d39f3b25e64a4e8e6d1
heysami <samiadji.fr@gmail.com>
2026-04-22 12:36:09 +0800

    Separate game AI threads from details

--- social/app-world.js
+++ social/app-world.js
@@ -48,7 +48,7 @@ import {
   createWorldGamesApi,
   createWorldGameLibrary,
   createWorldGameShell,
-} from "./world-games-ui.js?v=20260422gamelibraryrollback1";
+} from "./world-games-ui.js?v=20260422gamelibrarythreads1";
 
 const { fetchJson, formatRelativeTime, mauworldApiUrl } = window.MauworldSocial;
 
